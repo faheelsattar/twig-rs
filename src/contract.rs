589eda123e8b7a40fc5f8e1f500bc309e9a962d1
faheelsattar <faheelsattar@yahoo.com>
2023-01-14 14:22:32 +0500

    add Contract implementation

---
+++ src/contract.rs
@@ -0,0 +1,14 @@
+use ethers::types::Bytes;
+
+pub struct Contract {
+    pub bytecode: Bytes,
+}
+
+impl Contract {
+    pub fn new(mut self, bytecode: Bytes) ->  Self {
+        self.bytecode = bytecode;
+        self
+    }
+
+    pub fn get_dispatcher(&self) {}
+}
