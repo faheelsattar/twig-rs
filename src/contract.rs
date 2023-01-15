d3a91977dbb7058deb305759b46f8bab30b68314
faheelsattar <faheelsattar@yahoo.com>
2023-01-16 01:21:17 +0500

    add todo and doc

--- src/contract.rs
+++ src/contract.rs
@@ -10,35 +10,12 @@ impl Contract {
     }
 
     pub fn extract_dispatcher(self, pattern: Vec<u8>) {
-        search(pattern, self.bytecode)
+        search(pattern, self.bytecode);
     }
 }
 
-fn compute_lps_array(pattern: &Vec<u8>, m: usize) -> Vec<i32> {
-    let mut len = 0;
-    let mut i = 1;
-    let mut lps: Vec<i32> = vec![0];
-
-    loop {
-        if i >= m {
-            return lps;
-        }
-        println!("i {} m {}", i, m);
-        if pattern[i] == pattern[len] {
-            len += 1;
-            lps.push(len as i32);
-            i += 1;
-        } else {
-            if len != 0 {
-                len = lps[len - 1 as usize] as usize;
-            } else {
-                lps.push(len as i32);
-                i += 1;
-            }
-        }
-    }
-}
-
+// Todo: prolly move to some helper file
+/// helper function to search function signatures from the bytecode
 fn search(pattern: Vec<u8>, bytecode: Bytes) {
     let m = pattern.iter().len();
     let n = bytecode.iter().len();
@@ -70,12 +47,36 @@ fn search(pattern: Vec<u8>, bytecode: Bytes) {
         }
 
         if j == m {
-            println!("Signature 0x{}", signature);
+            println!("Signature {}", signature);
             j = lps[j - 1] as usize;
         } else if i < n && pattern[j] != bytecode[i] {
             if j != 0 {
                 j = lps[j - 1] as usize;
             } else {
+                i += 1;
+            }
+        }
+    }
+}
+
+fn compute_lps_array(pattern: &Vec<u8>, m: usize) -> Vec<i32> {
+    let mut len = 0;
+    let mut i = 1;
+    let mut lps: Vec<i32> = vec![0];
+
+    loop {
+        if i >= m {
+            return lps;
+        }
+        if pattern[i] == pattern[len] {
+            len += 1;
+            lps.push(len as i32);
+            i += 1;
+        } else {
+            if len != 0 {
+                len = lps[len - 1 as usize] as usize;
+            } else {
+                lps.push(len as i32);
                 i += 1;
             }
         }
